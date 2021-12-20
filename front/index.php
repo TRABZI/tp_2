d739cf0c99cf0477c78dca1ec9031067c972ec84
trabzi med amine <amine.trabzi@yahoo.com>
2021-12-20 14:42:52 +0000

    fix

--- front/index.php
+++ front/index.php
@@ -1,5 +1,5 @@
 <?php
-$dbh = new PDO('mysql:host=db;dbname=mysql', 'amine', 'amine');
+$dbh = new PDO('mysql:host=db;dbname=mysql', 'amine', 'passe');
 foreach($dbh->query('SHOW MYTABLE') as $row) {
 echo $row[0]. '<br/>';
 }
